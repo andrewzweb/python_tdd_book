4199ce30215c47030d2f72b6160db2cc5c7a4d27
andrewzweb <andrewzweb@gmail.com>
2019-08-20 13:30:04 +0300

    base view return minimal html

--- config_app/lists/views.py
+++ config_app/lists/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 
 # Создайте ваши представления здесь.
-def home_page():
+def home_page(request):
     '''домашняя страница'''
-    pass
+    template = '<html><title>To-Do lists</title></html>'
+    return HttpResponse(template)
+
