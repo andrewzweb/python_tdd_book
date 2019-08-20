76b6d2cbfc11653e3a57f40864b3cf3308eff5de
andrewzweb <andrewzweb@gmail.com>
2019-08-20 12:08:25 +0300

    added first FT in comment, and use unittest

--- config_app/functional_tests.py
+++ config_app/functional_tests.py
@@ -1,9 +1,28 @@
 
 
 from selenium import webdriver 
+import unittest 
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    """ test new user """
+
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_can_a_list_and_retrieve_it_later(self):
+        """ test : can start and get list """
+        self.browser.get("http://localhost:8000")
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('End test!')
+
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
 
