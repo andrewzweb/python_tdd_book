7acf5a177c742ba8143a203204aa0b6faff0902f
andrewzweb <andrewzweb@gmail.com>
2019-08-20 17:17:47 +0300

    refactoring: test separation, one test - one responsobility

--- config_app/lists/tests.py
+++ config_app/lists/tests.py
@@ -29,9 +29,13 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
+
+    def test_redirects_after_POST(self):
+        '''test: redirect after post-request'''
+        response = self.client.post('/', data={'item_text':'A new list item'})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
-
+        
 
     def test_only_saves_items_when_necessary(self):
         '''тест: сохраняет элементы, только когда нужно'''
