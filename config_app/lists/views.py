b6f6380dfca3e1096c49d481b926de69eb5fc55e
andrewzweb <andrewzweb@gmail.com>
2019-08-20 17:14:22 +0300

    add redirect after senf form

--- config_app/lists/views.py
+++ config_app/lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from lists.models import Item
 
@@ -6,8 +6,11 @@ from lists.models import Item
 def home_page(request):
     '''домашняя страница'''
 
-    item = Item()
-    item.text = request.POST.get('item_text','')
-    item.save()
-    return render(request, 'home.html', {'new_item_text' : request.POST.get('item_text')})
+    if request.method == "POST":
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+        return redirect('/')
+    else: 
+        new_item_text = ''
+    return render(request, 'home.html', {'new_item_text' : new_item_text })
 
