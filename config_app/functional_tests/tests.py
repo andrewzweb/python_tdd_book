5db667db84b312e353ee9c6430d4dfc20e812384
andrewzweb <andrewzweb@gmail.com>
2019-08-22 14:34:51 +0300

    add new test: multi user, in each user personal list

--- config_app/functional_tests/tests.py
+++ config_app/functional_tests/tests.py
@@ -38,7 +38,7 @@ class NewVisitorTest(LiveServerTestCase):
                 time.sleep(0.5)
 
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         '''test: can start list and get him later'''
         # Lusi hear about new app with lists
         # and want see this app 
@@ -54,22 +54,58 @@ class NewVisitorTest(LiveServerTestCase):
             'Enter a to-do item'
         )
         # They write in textfield "Buy something"
-        inputbox.send_keys('Buy something') 
+        inputbox.send_keys('buy peacock feathers') 
         # When she press enter, page  updated
         # now page consist  "Buy something"
 
         inputbox.send_keys(Keys.ENTER) 
-        self.wait_for_row_in_list_table("1: Buy something")
+        self.wait_for_row_in_list_table("1: buy peacock feathers")
 
 
         # Text field invites she add element 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys("Make")
+        inputbox.send_keys("make a fly")
         inputbox.send_keys(Keys.ENTER)
 
-        self.wait_for_row_in_list_table("1: Buy something")
-        self.wait_for_row_in_list_table("2: Make")
+        self.wait_for_row_in_list_table("1: buy peacock feathers")
+        self.wait_for_row_in_list_table("2: make a fly")
         
 
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        ''' test: multiple users can start lists at different urls'''
+        
+        # Lusi star tnew list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: buy peacock feathers')
+        # She see list have uniq url
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+') 
 
+        # Now user Feliks go to site 
+        ## use new seccion becouse we need insulation
+        ## info Lisi dont gone for cookie
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        # Feliks see home page no mark Lusi
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+        # Feliks start new list 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        # Feliks get uniq url 
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        # Again not mark Lusi
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
+        # satisfied go to bed
