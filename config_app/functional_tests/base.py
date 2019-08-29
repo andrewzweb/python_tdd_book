4fd82aa1767dffe8b703bc1596da7f80e8f724cc
andrewzweb <andrewzweb@gmail.com>
2019-08-29 13:56:45 +0300

    FT move in personal files

---
+++ config_app/functional_tests/base.py
@@ -0,0 +1,40 @@
+import os
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver 
+
+MAX_WAIT = 10
+
+class FunctionalTest(StaticLiveServerTestCase):
+    """ test new user """
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server: 
+            self.live_server_url = 'http://' + staging_server
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_can_a_list_and_retrieve_it_later(self):
+        """ test : can start and get list """
+        self.browser.get(self.live_server_url)
+        self.assertIn('To-Do', self.browser.title)
+
+    def wait_for_row_in_list_table(self, row_text):
+        ''' wait str in table '''
+
+        start_time = time.time()
+        while True:
+            try: 
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+
--- config_app/functional_tests/test_simple_list_creation.py
+++ config_app/functional_tests/test_simple_list_creation.py
@@ -1,47 +1,10 @@
-import os
-from django.test import LiveServerTestCase
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
-import time
-import unittest 
-
-MAX_WAIT = 10
-
-class NewVisitorTest(StaticLiveServerTestCase):
-    """ test new user """
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server: 
-            self.live_server_url = 'http://' + staging_server
-
-    def tearDown(self):
-        self.browser.quit()
 
 
-    def test_can_a_list_and_retrieve_it_later(self):
-        """ test : can start and get list """
-        self.browser.get(self.live_server_url)
-        self.assertIn('To-Do', self.browser.title)
-
-    def wait_for_row_in_list_table(self, row_text):
-        ''' wait str in table '''
-
-        start_time = time.time()
-        while True:
-            try: 
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
+class NewVisitorTest(FunctionalTest):
+    '''test new user'''
 
     def test_can_start_a_list_for_one_user(self):
         '''test: can start list and get him later'''
@@ -114,22 +77,5 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # satisfied go to bed
-        
-    def test_layout_and_styling(self):
-        ''' test layout and styling '''
-        
-        # Luci open hone page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-        
-        # she start new list  and see input in center 
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1: testing")
-        
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10
-        )
-        
+
+
