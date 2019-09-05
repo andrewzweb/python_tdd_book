b3df3d57bf497c1e8f8c1d9ad6ae89c24b8b341f
andrewzweb <andrewzweb@gmail.com>
2019-09-05 23:18:48 +0300

    cahnge statis setting app

--- config_app/config_app/settings.py
+++ config_app/config_app/settings.py
@@ -107,12 +107,12 @@ MIDDLEWARE = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 #PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(BASE_DIR, '../static')
+STATIC_ROOT  =   os.path.join(BASE_DIR, '../static/')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'lists/static/'),
 )
 
 #  Add configuration for static files storage using whitenoise
--- config_app/config_app/urls.py
+++ config_app/config_app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.conf.urls import url, include
 from lists import views as list_views
@@ -6,4 +8,4 @@ from lists import urls as list_urls
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
