a9a723f4ec019068850d2202f9bad1ce62baa4c3
andrewzweb <andrewzweb@gmail.com>
2019-09-05 22:35:12 +0300

    delete console.log from js

--- config_app/lists/static/js/list.js
+++ config_app/lists/static/js/list.js
@@ -1,10 +1,7 @@
-
 var initialize = function(){
-	console.log('initialize');
 	$('input[name="text"]').on('keypress', function(){
 		$('.has-error').hide();
 	});
 };
-console.log('list.js loaded');
 
 
