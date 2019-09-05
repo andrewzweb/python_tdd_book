f77ff2c659b326b069b1b4b473252ef387ddcdb6
andrewzweb <andrewzweb@gmail.com>
2019-09-05 18:54:35 +0300

    add new FT test

--- config_app/functional_tests/test_list_item_validation.py
+++ config_app/functional_tests/test_list_item_validation.py
@@ -4,6 +4,9 @@ from .base import FunctionalTest
 class ItemValidatorTest(FunctionalTest):
     ''' test valid new element '''
 
+    def get_error_element():
+        '''get error'''
+        return self.browser.find_element_by_css_selector('.has-error')
 
     def test_cannot_add_empty_list_items(self):
         '''test: can not use empty element  '''
@@ -51,9 +54,33 @@ class ItemValidatorTest(FunctionalTest):
 
         # home reload and show error messege 
         # what say element can not be dublicate
-        self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text,
+        self.wait_for(lambda: self.assertEqual(self.get_error_element().text,
                       "You've already got this in your list"
         ))
 
+    def test_error_messages_are_cleared_on_input(self):
+        '''test error messages are cleared on input'''
+
+        # Luci start new list 
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Banter too thick')
+
+        # accidentally input the same 
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # she start write and error hide 
+        self.get_item_input_box().send_keys('a')
+
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
+        ))
         
 
+        
