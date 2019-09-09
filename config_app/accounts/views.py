2592c0c72467cfcd7d5c4a9e0a90cd9c361ee3dd
andrewzweb <andrewzweb@gmail.com>
2019-09-09 10:42:02 +0300

    view which send email

--- config_app/accounts/views.py
+++ config_app/accounts/views.py
@@ -1,3 +1,24 @@
+import uuid
+import sys 
 from django.shortcuts import render
+from django.core.mail import send_mail
 
-# Create your views here.
+from accounts.models import Token 
+
+
+def send_login_email(request):
+    '''send link on email'''
+
+    email = request.POST['email']
+    uid = str(uuid.uuid4())
+    Token.objects.create(email=email, uid=uid)
+    
+    print('saving uid', uid, 'for mail', email, file=sys.stderr)
+    send_mail(
+        'You login link for Lists',
+        f'Use this link to log in:\n\n{url}',
+        'email_my_app@mail'
+        [email]
+    )
+    
+    return render(request, 'login_email_sent.html')
