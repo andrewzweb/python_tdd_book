a5053b29b4798c001358c7f14fb612a013db6b94
andrewzweb <andrewzweb@gmail.com>
2019-09-12 23:20:33 +0300

    add more test wirh mock

--- config_app/accounts/tests/test_views.py
+++ config_app/accounts/tests/test_views.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from unittest.mock import patch 
+from unittest.mock import patch, call
 
 class SendLoginEmailViewTest(TestCase):
     '''test view with send email for login in sys'''
@@ -16,4 +16,39 @@ class SendLoginEmailViewTest(TestCase):
         self.assertEqual(subject, 'Your login link for Superlists')
         self.assertEqual(from_email, 'noreply@superlists')
         self.assertEqual(to_list, ['luci@example.com'])
+
+    def test_adds_success_message(self):
+        '''test add masega about success'''
+
+        response = self.client.post('/accounts/send_login_email', 
+            data = {'email': 'luci@example.com'}, follow=True
+        )
+
+        message = list(response.context['messages'])[0]
+        
+        self.assertEqual(
+            message.message, 
+            'Check you message, we send You link, which can use login in site'
+        )
+        self.assertEqual(message.tags, 'success')
+
+    @patch('accounts.views.messages')
+    def test_adds_success_message_with_mocks(self, mock_messages):
+        '''test send messages'''
+
+        response = self.client.post(
+            '/accounts/send_login_email', 
+            data={'email':'luci@example.com'}
+        )
+        
+        expected = 'Check you message, we send You link, which can use login in site'
+        
+        self.assertEqual(
+            mock_messages.success.call_args, 
+            call(response.wsgi_request, expected)
+        )
+
+
+
+
 
--- config_app/accounts/views.py
+++ config_app/accounts/views.py
@@ -1,6 +1,7 @@
 import uuid
 import sys 
 from django.contrib.auth import authenticate
+from django.contrib import messages
 from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
@@ -8,14 +9,23 @@ from django.core.mail import send_mail
 from accounts.models import Token 
 
 
-def send_mail(request):
+def send_login_email(request):
     '''send link on email'''
     email = request.POST['email']
+    recipient_list = [email]
+
     send_mail(
         'Your login link for Superlists',
         'body text tbc',
         'noreply@superlists',
-        [email])
+        [email]
+    )
+
+    messages.add_message(
+        request,
+        messages.SUCCESS,
+        'Check you message, we send You link, which can use login in site'
+    )
     return redirect('/')
 
 
@@ -33,3 +43,4 @@ def logout(request):
     '''log out'''
     auth_logout(request)
     return redirect('/')
+
