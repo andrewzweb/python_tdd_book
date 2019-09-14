4c5deff3956bd788f8ac1964aaec36050c2214bd
andrewzweb <andrewzweb@gmail.com>
2019-09-14 14:58:01 +0300

    new FT for loggin

--- config_app/functional_tests/base.py
+++ config_app/functional_tests/base.py
@@ -56,3 +56,25 @@ class FunctionalTest(StaticLiveServerTestCase):
         ''' get input element '''
         
         return self.browser.find_element_by_id('id_text')
+
+
+    def wait_to_be_logged_in(self, email):
+        '''wait in sys '''
+        
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('Log Out')
+        )
+        
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(email, navbar.text)
+        
+    def wait_to_be_logged_out(self, email):
+        '''wait exit sys'''
+
+        self.wait_for(
+            lambda: self.browser.find_element_by_name('email')
+        )
+
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(email, navbar.text)
+
